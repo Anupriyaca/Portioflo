1c379dd3a204a9a8c8b483a1551121b6255e3bd5
Anupriyaca <161111477+Anupriyaca@users.noreply.github.com>
2024-02-28 23:56:54 +0530

    Add files via upload

---
+++ Commandline.java
@@ -0,0 +1,20 @@
+package week02;
+
+public class Commandline {
+	public static void main(String args[])
+	{
+		if(args.length>0)
+		{
+			System.out.println("Java Buzzwords....");
+			for(byte i=0;i<args.length;i++)
+			{
+				System.out.println((i+1)+"."+args[i]);
+			}
+		}
+		else 
+		{
+			System.out.println("NO COMMANDLINE ARGUMENT FOUND");
+		}
+	}
+
+}
